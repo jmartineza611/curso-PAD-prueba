8deafb1c58ebe80d624157eb2f46256c7d4bbcba
jmartineza611 <45548518+jmartineza611@users.noreply.github.com>
2018-12-03 00:39:09 -0600

    finish

--- src/components/InfoUser.vue
+++ src/components/InfoUser.vue
@@ -2,6 +2,22 @@
   <div>
     <h1>Nombre: {{name}} </h1>
     <h2>Trabajo: {{job}} </h2>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col lg="4"></b-col>
+        <b-col lg="4">
+          <b-form-input v-model="form.name"
+                        type="text"
+                        placeholder="Introduce tu nombre"></b-form-input>
+          <b-form-input v-model="form.job"
+                        type="text"
+                        placeholder="Introduce tu empleo"></b-form-input>
+          <b-button class="mt-5" @click="agregarUsuario()"> Agregar Usuario</b-button>
+        </b-col>
+        <b-col lg="4"></b-col>
+      </b-row>
+    </b-container>
+    <b-table class="mt-5" hover :items="users"></b-table>
   </div>
 </template>
 
@@ -13,14 +29,22 @@ export default {
   data() {
     return{
       name: '',
-      job: ''
+      job: '',
+      form: {
+        name: '',
+        job: ''
+      },
+      users: []
     }
   },
   methods: {
     async traerUser() {
       const resp = await UserService.getUser()
-      this.name = resp.data.name
-      this.job = resp.data.job
+      this.users = resp.data.users
+    },
+     async agregarUsuario() {
+      const response = await UserService.addUser(this.form)
+      alert(response.data.message)
     }
   },
   mounted(){
@@ -28,4 +52,3 @@ export default {
   }
 }
 </script>
-
